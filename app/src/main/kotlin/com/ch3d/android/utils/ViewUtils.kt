3600ded08e749c76d882ab4c4b2ca58f967138e4
Dmitry Chertenko <xch3dx@gmail.com>
2016-08-31 18:36:35 +0200

    small code cleanup

--- app/src/main/kotlin/com/ch3d/android/utils/ViewUtils.kt
+++ app/src/main/kotlin/com/ch3d/android/utils/ViewUtils.kt
@@ -2,15 +2,25 @@ package com.ch3d.android.utils
 
 import android.content.Context
 import android.util.TypedValue
+import android.view.View
 
-/**
- * TODO add javadocs
- */
 class ViewUtils {
     companion object {
         fun dpToPx(context: Context, i: Float): Float {
             val displayMetrics = context.resources.displayMetrics
             return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, i, displayMetrics)
         }
+
+        fun setViewVisibility(view: View?, visibility: Int) {
+            if (view?.visibility != visibility) {
+                view?.visibility = visibility
+            }
+        }
+
+        fun setViewGone(view: View?, gone: Boolean) =
+                setViewVisibility(view, if (gone) View.GONE else View.VISIBLE)
+
+        fun setViewInvisible(view: View?, gone: Boolean) =
+                setViewVisibility(view, if (gone) View.INVISIBLE else View.VISIBLE)
     }
 }
